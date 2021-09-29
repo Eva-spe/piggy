0427ec47aad7c2e4d4a336eeb732c77b5f6650c4
Eva-spe <monaevang@gmail.com>
2021-09-29 19:43:06 +0530

    3

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>My React Application</h1>
+        <h2>Author: Eva</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
